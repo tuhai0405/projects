8e16ab24524f709f3cd68932d4a306911f52e234
tuhai0405 <haitvpd07298@fpt.edu.vn>
2024-06-12 11:08:51 +0700

    deploy 3

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -3,6 +3,7 @@ import { ITask } from '../itask';
 import { CommonModule } from '@angular/common';
 
 import { RouterLink } from '@angular/router';
+import { DulieuService } from '../dulieu.service';
 @Component({
   selector: 'app-task-list',
   standalone: true,
@@ -11,13 +12,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent implements OnInit{
-  
+  constructor(private http:DulieuService){}
   list_task:ITask[]=[]
 ngOnInit(): void {
   fetch(`http://localhost:3000/task`).then(res=>res.json()).then ((data)=> {
     console.log('hello');
     this.list_task= data as ITask[];
   })
+}
+
+xoatask(id:any){
+this.http.xoaTask(id).subscribe((response)=>{
+  console.log('tuhaicobo');
+})
 }
   
   }
